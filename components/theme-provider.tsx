cb595a49a033a308cd7e96e386f034de65a906c5
harish <harishg.hiriyur@gmail.com>
2025-03-16 22:29:19 -0400

    theme provider fix

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,8 +1,13 @@
 "use client";
-
-import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { type ThemeProviderProps } from "next-themes";
+import type { ReactNode } from "react";
+
+interface ThemeProviderProps {
+  children: ReactNode;
+  attribute?: string;
+  defaultTheme?: string;
+  enableSystem?: boolean;
+}
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
